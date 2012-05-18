1441cec4eb4414fb45b10f0db8ad64eb4dba9bf8
Stephen Rees-Carter <stephen@rees-carter.net>
2012-05-18 15:35:44 +1000

    Using new View Helper registration method.

--- config/module.config.php
+++ config/module.config.php
@@ -6,28 +6,16 @@ return array(
                 'edpmarkdown_parser' => 'MarkdownExtra_Parser',
                 //'edpmarkdown_parser' => 'Markdown_Parser',
             ),
-            'Zend\View\HelperLoader' => array(
-                'parameters' => array(
-                    'map' => array(
-                        'markdown' => 'EdpMarkdown\View\Helper\Markdown',
-                    ),
-                ),
-            ),
-            'Zend\View\PhpRenderer' => array(
-                'parameters' => array(
-                    'broker' => 'Zend\View\HelperBroker',
-                ),
-            ),
-            'Zend\View\HelperBroker' => array(
-                'parameters' => array(
-                    'loader' => 'Zend\View\HelperLoader',
-                ),
-            ),
             'EdpMarkdown\View\Helper\Markdown' => array(
                 'parameters' => array(
                     'parser' => 'edpmarkdown_parser',
                 ),
             ),
+        ),
+    ),
+    'view_manager' => array(
+        'helper_map' => array(
+            'markdown' => 'EdpMarkdown\View\Helper\Markdown',
         ),
     ),
 );
