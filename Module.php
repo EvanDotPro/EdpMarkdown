371880280e81b26d122f0c3adbe7dc47fc0eba83
GianArb <gianarb92@gmail.com>
2013-12-04 15:39:51 +0100

    remove use in Module.php

--- Module.php
+++ Module.php
@@ -1,9 +1,6 @@
 <?php
 namespace EdpMarkdown;
 
-
-use Michelf\Markdown;
-
 class Module extends \Zend\View\Helper\AbstractHelper
 {
 
@@ -15,7 +12,7 @@ class Module extends \Zend\View\Helper\AbstractHelper
     public function __invoke($string = null)
     {
         if (file_exists(__DIR__.'/../../autoload.php')) {
-            return Markdown::defaultTransform($string);
+            return \Michelf\Markdown::defaultTransform($string);
         }
         require_once __DIR__ . '/php-markdown/markdown.php';
         return Markdown($string);
